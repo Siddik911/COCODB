c146b5dda1baeb9b2c914a744d7a703a83ad1d6c
Rafid <rafidabrar.47@gmail.com>
2023-08-07 14:27:53 +0600

    Customer ID done

--- first.sql
+++ first.sql
@@ -27,6 +27,16 @@ ADD COLUMN `emp_gender` VARCHAR(45) NULL AFTER `emp_name`,
 ADD COLUMN `emp_position` VARCHAR(45) NULL AFTER `emp_gender`,
 ADD COLUMN `emp_age` INT NULL AFTER `emp_position`;
 
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('01', 'Jhon', '56', 'Newyork');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('02', 'Dey', '34', 'Albama');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('03', 'Chris', '30', 'Texas');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('04', 'Nolan', '60', 'Newyork');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('05', 'Marc', '25', 'Dellas');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('06', 'Pau', '28', 'Buford');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('07', 'Emeka', '40', 'Albama');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('08', 'Sabrina', '22', 'Texas');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('09', 'Sofia', '27', 'Washington');
+INSERT INTO `Trial`.`cus_info` (`cus_id`, `cus_name`, `cus_age`, `cus_address`) VALUES ('10', 'Silva', '45', 'Texas');
 
 
 
